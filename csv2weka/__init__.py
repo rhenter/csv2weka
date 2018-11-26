6fd572ae72affaeb1e8ae9d3259e95d76e3fe5b3
Rafael Henter <rafael@henter.com.br>
2018-11-26 14:06:39 -0200

    Update converter file

--- csv2weka/__init__.py
+++ csv2weka/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from .analizer import Csv2Weka  # noqa
+from .converter import Csv2Weka  # noqa
 from .version import __version__  # noqa
 
 __all__ = ['Csv2Weka', '__version__']
--- csv2weka/main.py
+++ csv2weka/main.py
@@ -33,10 +33,16 @@ def print_version(ctx, param, value):
     help='ARFF file Name to use on Weka.',
 )
 @click.option(
-    'feature_cols', '-c',
+    'feature_cols', '-f',
     default='',
     prompt='Feature Columns',
     help='Feature Columns to OneHotEncoding.'
+)
+@click.option(
+    'categorical_cols', '-c',
+    default='',
+    prompt='Categorical Columns',
+    help='Columns that are categorical and wont be converted to One Hot Encoded column.'
 )
 @click.option(
     '--version',
@@ -46,7 +52,7 @@ def print_version(ctx, param, value):
     is_eager=True,
     help='Show the version and exit.'
 )
-def main(input_filename, output_filename, feature_cols):
+def main(input_filename, output_filename, feature_cols, categorical_cols):
     """
     Python command line tool to convert string data to numerical using
     OneHotEncoding to use on Weka Software
@@ -54,37 +60,34 @@ def main(input_filename, output_filename, feature_cols):
     \b
     Example:
     \b
-    \t $ csv2weka -i capture-sample.csv -o test.arff -c Protocol,State,Direction
+    \t $ csv2weka -i capture-sample.csv -o test.arff -f Protocol,State,Direction -c Label
 
     """
     click.echo('-' * 25)
 
-    # Instanciate the Csv2Web object
-    csv_path = os.path.abspath(input_filename)
-    csv2weka = Csv2Weka(csv_path)
+    # Instantiate the Csv2Weka object
+    csv2weka = Csv2Weka(input_filename)
 
-    # Criando o data frame
     click.echo('\nAnalyzing CSV data file and creating the data frame ...')
-    data_frame = csv2weka.create_dataframe()
+    dataframe = csv2weka.create_dataframe()
 
-    if not feature_cols:
-        feature_cols = data_frame.columns
-    else:
+    if feature_cols:
         feature_cols = feature_cols.split(',')
 
-    # Convertendo as Strings para valores numericos
-    click.echo('\nDataFrame Training and Adding One Hot Encoding to selected columns ...')
-    one_hot_encoding = csv2weka.generate_one_hot_encoding(
-        data_frame=data_frame,
-        feature_cols=feature_cols
-    )
+    if categorical_cols:
+        categorical_cols = categorical_cols.split(',')
+        dataframe = csv2weka.set_as_category(
+            dataframe, categorical_cols
+        )
 
-    # Exportando dados para um arquivo ARFF (WEKA)
+    click.echo('\nDataFrame Training and Adding One Hot Encoding to non numerical columns ...')
+    encoded_dataframe = csv2weka.encode(dataframe, feature_cols)
+
     click.echo('\nExporting data to ARFF file ...')
     output_path = csv2weka.dump(
-        data=one_hot_encoding,
+        data=encoded_dataframe,
         output_filename=output_filename,
-        feature_cols=feature_cols
+        columns=encoded_dataframe.columns
     )
 
     click.echo(
